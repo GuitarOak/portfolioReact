250f5b7c85ee6389fa414dd82fd73aafc59e1ba3
Emil Karlsson <emil.h.karlsson@gmail.com>
2022-11-18 15:15:01 +0100

    Main banner almost complete

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import { NavBar } from './Components/Navbar';
+import {Banner} from './Components/Banner'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
+      <Banner />
     </div>
-  );
+  ); 
 }
 
 export default App;
